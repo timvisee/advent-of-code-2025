4292ba336b685270886129ee58311943cef5ce63
timvisee <tim@visee.me>
2025-12-02 10:02:41 +0100

    Improve day02, use modulo instead

--- day02a/src/main.rs
+++ day02a/src/main.rs
@@ -1,6 +1,6 @@
-pub fn main() {
-    let mut buffer = itoa::Buffer::new();
+// Using modulo trick from <https://github.com/MizardX/AdventOfCode_2025/blob/main/src/day_02.rs>
 
+pub fn main() {
     let count = include_bytes!("../input.txt")
         .split(|b| b == &b',')
         .map(|n| {
@@ -10,13 +10,13 @@ pub fn main() {
             (iter.next().unwrap(), iter.next().unwrap())
         })
         .flat_map(|(a, b)| a..=b)
-        .filter(|n| {
-            let s = buffer.format(*n).as_bytes();
-            if !s.len().is_multiple_of(2) {
-                return false;
-            }
-            let (a, b) = s.split_at(s.len() / 2);
-            a == b
+        .filter(|n| match n {
+            10..=99 => n % 11 == 0,
+            1_000..=9_999 => n % 101 == 0,
+            100_000..=999_999 => n % 1_001 == 0,
+            10_000_000..=99_999_999 => n % 10_001 == 0,
+            1_000_000_000..=9_999_999_999 => n % 100_001 == 0,
+            _ => false,
         })
         .sum::<usize>();
 
--- day02b/src/main.rs
+++ day02b/src/main.rs
@@ -1,6 +1,6 @@
-pub fn main() {
-    let mut buffer = itoa::Buffer::new();
+// Using modulo trick from <https://github.com/MizardX/AdventOfCode_2025/blob/main/src/day_02.rs>
 
+pub fn main() {
     let count = include_bytes!("../input.txt")
         .split(|b| b == &b',')
         .map(|n| {
@@ -10,18 +10,19 @@ pub fn main() {
             (iter.next().unwrap(), iter.next().unwrap())
         })
         .flat_map(|(a, b)| a..=b)
-        .filter(|n| {
-            let s = buffer.format(*n).as_bytes();
-            for len in 1..=s.len() / 2 {
-                if !s.len().is_multiple_of(len) {
-                    continue;
-                }
-                let (needle, haystack) = s.split_at(len);
-                if haystack.chunks(len).all(|chunk| chunk == needle) {
-                    return true;
-                }
+        .filter(|n| match n {
+            10..=99 => n % 11 == 0,
+            100..=999 => n % 111 == 0,
+            1_000..=9_999 => n % 101 == 0 || n % 1_111 == 0,
+            10_000..=99_999 => n % 11_111 == 0,
+            100_000..=999_999 => n % 1_001 == 0 || n % 10_101 == 0 || n % 111_111 == 0,
+            1_000_000..=9_999_999 => n % 1_111_111 == 0,
+            10_000_000..=99_999_999 => n % 10_001 == 0 || n % 1_010_101 == 0 || n % 11_111_111 == 0,
+            100_000_000..=999_999_999 => n % 1_001_001 == 0 || n % 111_111_111 == 0,
+            1_000_000_000..=9_999_999_999 => {
+                n % 100_001 == 0 || n % 101_010_101 == 0 || n % 1_111_111_111 == 0
             }
-            false
+            _ => false,
         })
         .sum::<usize>();
 
