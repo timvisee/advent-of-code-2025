62a45bf6fd5db1fafd3d1d276eae8c7d8b3851fa
timvisee <tim@visee.me>
2025-12-04 10:20:19 +0100

    Optimize day04

--- day04a/src/main.rs
+++ day04a/src/main.rs
@@ -1,39 +1,26 @@
+#[rustfmt::skip]
+const A: [(isize, isize); 8] = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)];
+
 pub fn main() {
-    #[rustfmt::skip]
-    const ADJACENT: [(isize, isize); 8] = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)];
-
-    let map = include_bytes!("../input.txt")
+    let m = include_bytes!("../input.txt")
         .split(|b| b == &b'\n')
-        .collect::<Vec<&[u8]>>();
+        .collect::<Vec<_>>();
 
-    let mut total = 0;
-
-    for x in 0..map[0].len() as isize {
-        for y in 0..map.len() as isize {
-            let cell = map[y as usize][x as usize];
-
-            if cell == b'.' {
-                continue;
-            }
-
-            let rolls = ADJACENT
-                .into_iter()
-                .filter(|(ax, ay)| {
-                    let xx = x + ax;
-                    let yy = y + ay;
-
-                    map.get(yy as usize)
-                        .and_then(|row| row.get(xx as usize))
-                        .is_some_and(|&c| c == b'@')
-                })
-                .take(4)
-                .count();
-
-            if rolls < 4 {
-                total += 1;
-            }
-        }
-    }
-
-    println!("{total}");
+    println!(
+        "{}",
+        (0..m[0].len())
+            .flat_map(|x| (0..m.len()).map(move |y| (x, y)))
+            .filter(|&(x, y)| {
+                m[y][x] == b'@'
+                    && A.into_iter()
+                        .filter(|&(ax, ay)| {
+                            m.get(y.wrapping_add_signed(ay))
+                                .and_then(|row| row.get(x.wrapping_add_signed(ax)))
+                                .is_some_and(|&c| c == b'@')
+                        })
+                        .count()
+                        < 4
+            })
+            .count(),
+    );
 }
--- day04b/src/main.rs
+++ day04b/src/main.rs
@@ -1,7 +1,7 @@
-pub fn main() {
-    #[rustfmt::skip]
-    const ADJACENT: [(isize, isize); 8] = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)];
+#[rustfmt::skip]
+const A: [(isize, isize); 8] = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)];
 
+pub fn main() {
     let mut m = include_bytes!("../input.txt")
         .split(|b| b == &b'\n')
         .map(<[u8]>::to_vec)
@@ -11,37 +11,25 @@ pub fn main() {
     let mut queue = Vec::from_iter((0..m[0].len()).flat_map(|x| (0..m.len()).map(move |y| (x, y))));
 
     while let Some((x, y)) = queue.pop() {
-        if m[y][x] == b'.' {
-            continue;
-        }
-
-        if ADJACENT
-            .into_iter()
-            .filter(|(ax, ay)| {
-                m.get((y as isize + ay) as usize)
-                    .and_then(|row| row.get((x as isize + ax) as usize))
-                    .is_some_and(|&c| c == b'@')
-            })
-            .count()
-            >= 4
-        {
-            continue;
-        }
-
-        total += 1;
-        m[y][x] = b'.';
-
-        queue.extend(
-            ADJACENT
-                .into_iter()
-                .map(|(ax, ay)| ((ax + x as isize) as usize, (ay + y as isize) as usize))
-                .filter(|&(xx, yy)| {
-                    m.get(yy)
-                        .and_then(|row| row.get(xx))
+        if m[y][x] == b'@'
+            && A.into_iter()
+                .filter(|&(ax, ay)| {
+                    m.get(y.wrapping_add_signed(ay))
+                        .and_then(|row| row.get(x.wrapping_add_signed(ax)))
                         .is_some_and(|&c| c == b'@')
-                }),
-        );
+                })
+                .count()
+                < 4
+        {
+            total += 1;
+            m[y][x] = b'.';
+            queue.extend(
+                A.into_iter()
+                    .map(|(ax, ay)| (x.wrapping_add_signed(ax), y.wrapping_add_signed(ay)))
+                    .filter(|&(xx, yy)| m.get(yy).and_then(|row| row.get(xx)).is_some()),
+            );
+        }
     }
 
-    println!("{total}"); // 8317
+    println!("{total}");
 }
