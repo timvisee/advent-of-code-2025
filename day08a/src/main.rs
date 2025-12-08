37f76f0016614519727158794793aa289dfd7178
timvisee <tim@visee.me>
2025-12-08 10:57:51 +0100

    Add day08a and day08b

---
+++ day08a/src/main.rs
@@ -0,0 +1,40 @@
+type Coord = [usize; 3];
+
+pub fn main() {
+    let coords = include_bytes!("../input.txt")
+        .split(|&b| b == b'\n')
+        .map(|line| {
+            let mut nums = line.split(|&b| b == b',');
+            std::array::from_fn(|_| atoi::atoi::<usize>(nums.next().unwrap()).unwrap())
+        })
+        .collect::<Vec<_>>();
+
+    let mut edges = Vec::with_capacity(coords.len() * coords.len() / 2);
+    for i in 0..coords.len() - 1 {
+        for j in i + 1..coords.len() {
+            edges.push((i, j, dist(coords[i], coords[j])));
+        }
+    }
+    edges.sort_unstable_by_key(|&(_, _, dist)| dist);
+
+    let mut dsu = aph_disjoint_set::DisjointSet::new(1000);
+    edges.into_iter().take(1000).for_each(|(a, b, _)| {
+        dsu.union(a, b);
+    });
+
+    let mut circuits = (0..coords.len())
+        .fold(std::collections::HashMap::new(), |mut acc, i| {
+            *acc.entry(dsu.get_root(i)).or_default() += 1;
+            acc
+        })
+        .into_values()
+        .collect::<Vec<_>>();
+    circuits.sort_unstable_by_key(|&circuit| std::cmp::Reverse(circuit));
+
+    println!("{}", circuits[0..3].iter().product::<usize>());
+}
+
+#[inline]
+fn dist(a: Coord, b: Coord) -> usize {
+    a[0].abs_diff(b[0]).pow(2) + a[1].abs_diff(b[1]).pow(2) + a[2].abs_diff(b[2]).pow(2)
+}
---
+++ day08b/src/main.rs
@@ -0,0 +1,33 @@
+type Coord = [usize; 3];
+
+pub fn main() {
+    let coords = include_bytes!("../input.txt")
+        .split(|&b| b == b'\n')
+        .map(|line| {
+            let mut nums = line.split(|&b| b == b',');
+            std::array::from_fn(|_| atoi::atoi::<usize>(nums.next().unwrap()).unwrap())
+        })
+        .collect::<Vec<_>>();
+
+    let mut edges = Vec::with_capacity(coords.len() * coords.len() / 2);
+    for i in 0..coords.len() - 1 {
+        for j in i + 1..coords.len() {
+            edges.push((i, j, dist(coords[i], coords[j])));
+        }
+    }
+    edges.sort_unstable_by_key(|&(_, _, dist)| dist);
+
+    let mut dsu = aph_disjoint_set::DisjointSet::new(coords.len());
+    let (a, b, _) = edges
+        .into_iter()
+        .filter(|&(a, b, _)| matches!(dsu.union(a, b), aph_disjoint_set::UnionResult::Success))
+        .last()
+        .unwrap();
+
+    println!("{}", coords[a][0] * coords[b][0]);
+}
+
+#[inline]
+fn dist(a: Coord, b: Coord) -> usize {
+    a[0].abs_diff(b[0]).pow(2) + a[1].abs_diff(b[1]).pow(2) + a[2].abs_diff(b[2]).pow(2)
+}
