b9e2cf2290d415a11f241664888c0afd033d44f7
timvisee <tim@visee.me>
2025-12-12 11:13:18 +0100

    Optimize day12a, ignore shapes

--- day12a/src/main.rs
+++ day12a/src/main.rs
@@ -1,48 +1,24 @@
-use itertools::Itertools;
-
 pub fn main() {
-    let mut lines = include_bytes!("../input.txt").split(|&b| b == b'\n');
-
-    let shape_sizes = lines
-        .by_ref()
-        .take(6 * 5)
-        .tuples()
-        .map(|(_, a, b, c, _)| {
-            a.into_iter().filter(|&&b| b != b'#').count()
-                + b.into_iter().filter(|&&b| b != b'#').count()
-                + c.into_iter().filter(|&&b| b != b'#').count()
-        })
-        .collect::<Vec<usize>>();
-
     println!(
         "{}",
-        lines
-            .into_iter()
+        include_bytes!("../input.txt")
+            .split(|&b| b == b'\n')
+            .skip(6 * 5)
             .map(|region| {
                 let (size, counts) =
                     region.split_at(region.iter().position(|b| *b == b':').unwrap());
-                let area = size
-                    .split(|&b| b == b'x')
-                    .map(|n| atoi::atoi::<usize>(n).unwrap())
-                    .product::<usize>();
-                let counts = counts[2..]
-                    .split(|&b| b == b' ')
-                    .map(|n| atoi::atoi::<usize>(n).unwrap())
-                    .collect::<Vec<_>>();
-                (area, counts)
+                (
+                    size.split(|&b| b == b'x')
+                        .map(|n| atoi::atoi::<usize>(n).unwrap())
+                        .product::<usize>(),
+                    counts[3..]
+                        .split(|&b| b == b' ')
+                        .map(|n| atoi::atoi::<usize>(n).unwrap())
+                        .collect::<arrayvec::ArrayVec<_, 6>>(),
+                )
             })
             .filter(|(area, counts)| {
-                let min_cells = counts
-                    .iter()
-                    .zip(&shape_sizes)
-                    .map(|(count, shape_size)| count * shape_size)
-                    .sum::<usize>();
-                if *area < min_cells {
-                    return false;
-                }
-
-                let max_cells = counts.iter().map(|&count| count * 9).sum::<usize>();
-                *area >= max_cells
+                *area >= counts.iter().map(|&count| count * 9).sum::<usize>()
             })
             .count()
     );
